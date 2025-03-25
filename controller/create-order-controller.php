5805870fe657ed6f94231ff43814bd26373d499e
Araya RAKSACHOB <araya.raksachob@lapiscine.pro>
2025-03-25 14:16:15 +0100

    initialisation

---
+++ controller/create-order-controller.php
@@ -0,0 +1,4 @@
+<?php
+//Je fais appel à la page view (create-order) 
+// cele permet de faire appel (si besoin)à cette page plusieurs fois sans réécrire tous les codes html de cette dernière
+require_once("create-order-view.php");
---
+++ view/create-order-view.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shop-Tees</title>
+</head>
+
+<body>
+    <main>
+        <h2>Shop-Tees</h2>
+
+        <form action="" method="get">
+            <label for="">Quantité
+                <input type="number" name="quantity" id="">
+            </label>
+        </form>
+    </main>
+    
+</body>
+
+</html>
